738d6bb57af4affa29811d10d0b0ac0d0d7a12fd
Ben Locke <benlocke@outlook.com.au>
2018-12-18 20:10:36 +0800

    margin padding display fixes

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,31 +1,45 @@
 <template>
-  <v-container fill-height fluid>
-    <v-layout justify-center align-center>
+  <v-container fluid fill-height>
+    <v-layout align-center justify-center fill-height>
       <v-flex text-xs-center fill-height>
-        <v-card dark color="#36363f" height="100%">
+        <v-card dark tile
+                class="elevation-10"
+                color="#36363f"
+                height="100%"
+                max-height="720px"
+                max-width="720px"
+                style="margin: auto;"
+          >
           <v-toolbar dark color="#474755">
             <v-toolbar-title> Login </v-toolbar-title>
           </v-toolbar>
-          <v-icon size="256px" class="pa-3"> mdi-account-circle </v-icon>
+          <v-icon v-show="$vuetify.breakpoint.smAndDown"
+                  size="128px"
+                  class="pa-3">
+            mdi-account-circle
+          </v-icon>
+          <v-icon v-show="$vuetify.breakpoint.mdAndUp"
+                  size="256px"
+                  class="pa-3">
+            mdi-account-circle
+          </v-icon>
           <div class="login">
-            <v-text-field
-              v-model="user.email"
-              label="Email"
-              name="email"
-              class="pa-3"
-              light
-              required
-              solo
+            <v-text-field v-model="user.email"
+                          label="Email"
+                          name="email"
+                          class="auto"
+                          light
+                          required
+                          solo
             ></v-text-field>
-            <v-text-field
-              v-model="user.password"
-              label="Password"
-              name="password"
-              type="password"
-              class="pa-3"
-              light
-              required
-              solo
+            <v-text-field v-model="user.password"
+                          label="Password"
+                          name="password"
+                          type="password"
+                          class="auto"
+                          light
+                          required
+                          solo
             ></v-text-field>
           </div>
           <v-btn light round class="ma-2">Login</v-btn>
@@ -51,7 +65,7 @@ export default {
 
 <style lang="css" scoped>
   .login {
-    margin-left: 15%;
-    margin-right: 15%;
+    margin-left: 10vh;
+    margin-right: 10vh;
   }
 </style>
