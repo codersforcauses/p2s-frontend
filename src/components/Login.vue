de861a1dd689946e03709ecbedb3ca8caba91d63
blocke98 <benlocke@outlook.com.au>
2018-12-18 16:22:17 +0800

    cleanup and fixes

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,41 +1,39 @@
 <template>
-  <div id="login">
-    <v-app style="background: #212121;">
-      <v-container fluid fill-height>
-        <v-layout align-center justify-center>
-          <v-flex fill-height text-xs-center>
-            <v-card dark color="#36363f" class="elevation-10" height="100%">
-              <v-toolbar dark color="#474755">
-                  <v-toolbar-title>Login</v-toolbar-title>
-              </v-toolbar>
-              <v-icon size= "256px" class="pa-3">mdi-account-circle</v-icon>
-              <v-text-field
-                class="pa-3"
-                label="Email"
-                name="email"
-                prepend-icon="mdi-account"
-                v-model="user.email"
-                required
-                solo
-              ></v-text-field>
-              <v-text-field
-                class="pa-3"
-                label="Password"
-                name="password"
-                prepend-icon="mdi-lock"
-                type="password"
-                v-model="user.password"
-                required
-                solo
-              ></v-text-field>
-              <v-btn class="ma-2">Login</v-btn>
-              <v-btn class="ma-2">Forgot Password</v-btn>
-            </v-card>
-          </v-flex>
-        </v-layout>
-      </v-container>
-    </v-app>
-  </div>
+  <v-container fill-height fluid>
+    <v-layout justify-center align-center>
+      <v-flex text-xs-center fill-height>
+        <v-card dark color="#36363f" height="100%">
+          <v-toolbar dark color="#474755">
+            <v-toolbar-title> Login </v-toolbar-title>
+          </v-toolbar>
+          <v-icon size="256px" class="pa-3"> mdi-account-circle </v-icon>
+          <div class="login">
+            <v-text-field
+              v-model="user.email"
+              label="Email"
+              name="email"
+              class="pa-3"
+              light
+              required
+              solo
+            ></v-text-field>
+            <v-text-field
+              v-model="user.password"
+              label="Password"
+              name="password"
+              type="password"
+              class="pa-3"
+              light
+              required
+              solo
+            ></v-text-field>
+          </div>
+          <v-btn light round class="ma-2">Login</v-btn>
+          <v-btn light round class="ma-2">Forgot Password</v-btn>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
@@ -50,3 +48,10 @@ export default {
   },
 };
 </script>
+
+<style lang="css" scoped>
+  .login {
+    margin-left: 15%;
+    margin-right: 15%;
+  }
+</style>
