d2603c717abbe44c18576b0b8460606bb7111b49
Jeremiah Pinto <jeremiahpinto279@outlook.com>
2018-12-19 15:25:50 +0800

    Added files to create custom theme

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,17 +1,9 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify';
 import 'vuetify/dist/vuetify.min.css';
+import '@/assets/stylus/main.styl';
 
 Vue.use(Vuetify, {
-  theme: {
-    primary: '#ee44aa',
-    secondary: '#424242',
-    accent: '#82B1FF',
-    error: '#FF5252',
-    info: '#2196F3',
-    success: '#4CAF50',
-    warning: '#FFC107',
-  },
   options: {
     customProperties: true,
   },
