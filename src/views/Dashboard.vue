6c8cea8a4898b8a9c4fb5f9855459a9fed44c2a1
Jeremiah Pinto <jeremiahpinto279@outlook.com>
2018-12-20 12:26:31 +0800

    Updates more style variables

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,12 +1,79 @@
 <template>
-  <div>
-    <img alt="Vue logo" src="../assets/logo.png">
-  </div>
+  <v-content>
+    <v-toolbar flat>
+      <v-toolbar-title class="display-1"> Dashboard</v-toolbar-title>
+    </v-toolbar>
+    <v-card>
+      <h1 class="display-1">
+        Hello
+      </h1>
+      <p class="subheading">
+        hello
+      </p>
+
+      <v-list two-line>
+          <template v-for="(item, index) in items">
+            <v-subheader
+              v-if="item.header"
+              :key="item.header"
+            >
+              {{ item.header }}
+            </v-subheader>
+
+            <v-divider
+              v-else-if="item.divider"
+              :inset="item.inset"
+              :key="index"
+            ></v-divider>
+
+            <v-list-tile
+              v-else
+              :key="item.title"
+              avatar
+              @click="item.title"
+            >
+              <v-list-tile-avatar>
+                <img :src="item.avatar">
+              </v-list-tile-avatar>
+
+              <v-list-tile-content>
+                <v-list-tile-title v-html="item.title"></v-list-tile-title>
+                <v-list-tile-sub-title v-html="item.subtitle"></v-list-tile-sub-title>
+              </v-list-tile-content>
+            </v-list-tile>
+          </template>
+        </v-list>
+    </v-card>
+  </v-content>
 </template>
 
 <script>
 
 export default {
   name: 'dashboard',
+  data() {
+    return {
+      items: [
+        { header: 'Today' },
+        {
+          avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
+          title: 'Brunch this weekend?',
+          subtitle: "<span class='text--primary'>Ali Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?",
+        },
+        { divider: true, inset: true },
+        {
+          avatar: 'https://cdn.vuetifyjs.com/images/lists/2.jpg',
+          title: 'Summer BBQ <span class="grey--text text--lighten-1">4</span>',
+          subtitle: "<span class='text--primary'>to Alex, Scott, Jennifer</span> &mdash; Wish I could come, but I'm out of town this weekend.",
+        },
+        { divider: true, inset: true },
+        {
+          avatar: 'https://cdn.vuetifyjs.com/images/lists/3.jpg',
+          title: 'Oui oui',
+          subtitle: "<span class='text--primary'>Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?",
+        },
+      ],
+    };
+  },
 };
 </script>
