015ca95f312bac0fb893ee161bd12c5f71358112
Jeremiah Pinto <jeremiahpinto279@outlook.com>
2018-12-21 22:08:33 +0800

    Dynamic page titles

--- src/router.js
+++ src/router.js
@@ -1,35 +1,82 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Login from './views/Login.vue';
 
-const Dashboard = () => import(/* webpackChunkName: "dashboard" */ './views/Dashboard.vue');
+// Login page
+const Login = () => import(/* webpackChunkName: "login" */ './views/Login.vue');
+
+// Dashboards
+const Dashboard = import(/* webpackChunkName: "dashboard" */ './views/Dashboard.vue');
 
 Vue.use(Router);
 
-export default new Router({
+const routes = [
+  {
+    path: '/login',
+    name: 'login',
+    component: Login,
+    meta: {
+      title: 'Login',
+    },
+  },
+  {
+    path: '/',
+    name: 'dashboard',
+    component: Dashboard,
+    meta: {
+      title: 'Dashboard',
+    },
+    // children: [
+    //   {},
+    // ],
+  },
+];
+
+const router = new Router({
+  routes,
   mode: 'history',
   base: process.env.BASE_URL,
-  routes: [
-    {
-      path: '/login',
-      name: 'login',
-      component: Login,
-    },
-    {
-      path: '/',
-      name: 'dashboard',
-      component: Dashboard,
-      // children: [
-      //   {},
-      // ],
-    },
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (about.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
-    // },
-  ],
 });
+
+// This callback runs before every route change, including on page load.
+// eslint-disable-next-line consistent-return
+router.beforeEach((to, from, next) => {
+  // This goes through the matched routes from last to first,
+  // finding the closest route with a title.
+  // eg. if we have /some/deep/nested/route and
+  // /some, /deep, and /nested have titles, nested's will be chosen.
+  const nearestWithTitle = to.matched.slice().reverse().find(r => r.meta && r.meta.title);
+
+  // Find the nearest route element with meta tags.
+  const nearestWithMeta = to.matched.slice().reverse().find(r => r.meta && r.meta.metaTags);
+  // eslint-disable-next-line
+  const previousNearestWithMeta = from.matched.slice().reverse().find(r => r.meta && r.meta.metaTags);
+
+  // If a route with a title was found, set the document (page) title to that value.
+  if (nearestWithTitle) document.title = nearestWithTitle.meta.title;
+
+  // Remove any stale meta tags from the document using the key attribute we set below.
+  Array.from(document.querySelectorAll('[data-vue-router-controlled]')).map(el => el.parentNode.removeChild(el));
+
+  // Skip rendering meta tags if there are none.
+  if (!nearestWithMeta) return next();
+
+  // Turn the meta tag definitions into actual elements in the head.
+  nearestWithMeta.meta.metaTags.map((tagDef) => {
+    const tag = document.createElement('meta');
+
+    Object.keys(tagDef).forEach((key) => {
+      tag.setAttribute(key, tagDef[key]);
+    });
+
+    // We use this to track which meta tags we create, so we don't interfere with other ones.
+    tag.setAttribute('data-vue-router-controlled', '');
+
+    return tag;
+  })
+  // Add the meta tags to the document head.
+    .forEach(tag => document.head.appendChild(tag));
+
+  next();
+});
+
+export default router;
