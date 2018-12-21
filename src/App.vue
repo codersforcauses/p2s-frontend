4212d2150c5d02332e6e9d7a9d2b98c130ee0d47
Jeremiah Pinto <jeremiahpinto279@outlook.com>
2018-12-21 21:20:18 +0800

    Added service workers with app notifications for updates

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,77 @@
 <template>
   <v-app>
+    <v-snackbar bottom
+                left
+                v-model="snackWithButtons"
+                :timeout="timeout"
+                class="snack"
+    >
+      {{ snackWithBtnText }}
+      <v-spacer/>
+      <v-btn  dark
+              flat
+              color="info"
+              @click.native="refreshApp"
+      >
+        {{ snackBtnText }}
+      </v-btn>
+      <v-btn  icon
+              @click="snackWithButtons = false"
+      >
+        <v-icon> mdi-close </v-icon>
+      </v-btn>
+      </v-snackbar>
     <router-view/>
   </v-app>
 </template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      refreshing: false,
+      registration: null,
+      snackBtnText: '',
+      snackWithBtnText: '',
+      snackWithButtons: false,
+      timeout: 0,
+    };
+  },
+  created() {
+    // Listen for swUpdated event and display refresh snackbar as required.
+    document.addEventListener('swUpdated', this.showRefreshUI, { once: true });
+    // Refresh all open app tabs when a new service worker is installed.
+    navigator.serviceWorker.addEventListener('controllerchange', () => {
+      if (this.refreshing) return;
+      this.refreshing = true;
+      window.location.reload();
+    });
+  },
+  methods: {
+    showRefreshUI(e) {
+      // Display a snackbar inviting the user to refresh/reload the app due
+      // to an app update being available.
+      // The new service worker is installed, but not yet active.
+      // Store the ServiceWorkerRegistration instance for later use.
+      this.registration = e.detail;
+      this.snackBtnText = 'Refresh';
+      this.snackWithBtnText = 'New version available!';
+      this.snackWithButtons = true;
+    },
+    refreshApp() {
+      this.snackWithButtons = false;
+      // Protect against missing registration.waiting.
+      if (!this.registration || !this.registration.waiting) { return; }
+      this.registration.waiting.postMessage('skipWaiting');
+    },
+  },
+};
+</script>
+
+<style>
+/* Provide better right-edge spacing when using an icon button there. */
+.snack >>> .v-snack__content {
+  padding-right: 16px;
+}
+</style>
---
+++ src/sw.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable no-restricted-globals */
+// This is the code piece that GenerateSW mode can't provide for us.
+// This code listens for the user's confirmation to update the app.
+self.addEventListener('message', (e) => {
+  if (!e.data) {
+    return;
+  }
+
+  switch (e.data) {
+    case 'skipWaiting': self.skipWaiting(); break;
+    default: break;
+  }
+});
+
+workbox.clientsClaim();
+
+// The precaching code provided by Workbox.
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
