05ed8ab54c77f3c69e70d850f6a3d797f16a131c
Jeremiah Pinto <jeremiahpinto279@outlook.com>
2018-12-24 13:14:25 +0800

    Added logout functionaity

--- src/other/Toolbar.vue
+++ src/other/Toolbar.vue
@@ -10,26 +10,48 @@
               :icon="$vuetify.breakpoint.smAndDown"
               @click="$emit('dark')"
       >
-        <v-icon :left="$vuetify.breakpoint.mdAndUp"> mdi-theme-light-dark </v-icon>
-        <span v-show="$vuetify.breakpoint.mdAndUp"> {{ theme }} Theme </span>
+        <v-icon :left="$vuetify.breakpoint.mdAndUp">
+          mdi-theme-light-dark
+        </v-icon>
+
+        <span v-show="$vuetify.breakpoint.mdAndUp">
+          {{ theme }} Theme
+        </span>
       </v-btn>
 
       <v-btn  :flat="$vuetify.breakpoint.mdAndUp"
               :icon="$vuetify.breakpoint.smAndDown"
+              @click="logout"
       >
-        <span v-show="$vuetify.breakpoint.mdAndUp"> Logout </span>
-        <v-icon :right="$vuetify.breakpoint.mdAndUp"> mdi-logout </v-icon>
+        <span v-show="$vuetify.breakpoint.mdAndUp">
+          Logout
+        </span>
+
+        <v-icon :right="$vuetify.breakpoint.mdAndUp">
+          mdi-logout
+        </v-icon>
       </v-btn>
     </v-toolbar-items>
   </v-toolbar>
 </template>
 
 <script>
+import { mapActions } from 'vuex';
+
 export default {
   props: ['dark'],
   computed: {
     theme() {
       return this.dark ? 'Light' : 'Dark';
+    },
+  },
+  methods: {
+    ...mapActions('auth', { authLogout: 'logout' }),
+    logout() {
+      this.authLogout()
+        .then(() => {
+          this.$router.push('/');
+        });
     },
   },
 };
