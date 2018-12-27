36724362441fa0b25dd5087fc524050ce908020d
Ben Locke <benlocke@outlook.com.au>
2018-12-27 09:14:00 +0800

    completed desktop login

--- src/components/LoginDesktop.vue
+++ src/components/LoginDesktop.vue
@@ -7,13 +7,60 @@
                   color="#c22032"
                   height="100%"
           >
+          <v-img  :src="require('@/assets/p2s_massive.png')"
+                  style="padding-top: 65vh; margin: auto;"
+                  alt="P2S Rugbyworks Logo"
+                  max-width="350"
+                  contain
+            ></v-img>
           </v-card>
         </v-flex>
         <v-flex style="height: 100%" xs12>
-          <v-card light
+          <v-card dark
                   color="#272727"
                   height="100%"
           >
+            <v-flex xs6 offset-xs3 style="padding-top: 30vh;">
+              <v-form v-model="valid">
+                <label> Email </label>
+                <v-text-field :rules="emailRules"
+                              v-model="user.email"
+                              background-color="#cecece"
+                              autofocus
+                              flat
+                              light
+                              solo
+                ></v-text-field>
+                <label> Password </label>
+                <v-text-field v-model="user.password"
+                              type="password"
+                              background-color="#cecece"
+                              flat
+                              light
+                              required
+                              solo
+                ></v-text-field>
+            </v-form>
+            </v-flex>
+            <v-flex class="text-xs-center">
+              <v-btn class="ma-2"
+                     color="#cecece"
+                     depressed
+                     large
+                     light
+                     round
+              >
+                Login
+              </v-btn>
+              <v-spacer/>
+              <v-btn class="ma-2"
+                     color="grey lighten-1"
+                     flat
+                     small
+              >
+                Forgot Password?
+              </v-btn>
+            </v-flex>
           </v-card>
         </v-flex>
       </v-layout>
