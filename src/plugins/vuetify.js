1b499c0b3d88b74564310428b053d0835cec5029
Jeremiah Pinto <jeremiahpinto279@outlook.com>
2018-12-27 17:30:53 +0800

    Added theme caching

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,9 +1,17 @@
+/* eslint-disable import/no-unresolved */
 import Vue from 'vue';
 import Vuetify from 'vuetify';
+import LRU from 'lru-cache';
 import '@/assets/stylus/main.styl';
+
+const themeCache = LRU({
+  max: 10,
+  maxAge: 1000 * 60 * 60, // 1 hour
+});
 
 Vue.use(Vuetify, {
   options: {
+    themeCache,
     customProperties: true,
   },
   iconfont: 'mdi',
