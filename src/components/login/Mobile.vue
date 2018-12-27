9bd7cb62d39e042383f10eeca511600e24293269
Ben Locke <benlocke@outlook.com.au>
2018-12-27 16:45:36 +0800

    fix login form (JeremiahPinto)

---
+++ src/components/login/Mobile.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-content>
+    <v-toolbar app dark flat color="#c22032">
+      <v-toolbar-title>
+        Login
+      </v-toolbar-title>
+    </v-toolbar>
+
+    <v-container grid-list-lg fluid fill-height style="background-color: #c22032;">
+      <v-layout fill-height row wrap>
+        <v-flex xs12>
+          <v-card color="#ebebeb" class="pa-4" style="margin: -16px" height="100%">
+            <v-img  contain
+                    class="mx-auto py-5"
+                    max-width="32vmax"
+                    :src="require('@/assets/p2s_massive.png')"
+            ></v-img>
+            <slot name="form"></slot>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-content>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      valid: false,
+      show: false,
+      rules: {
+        required: v => !!v || 'This field is required',
+        email: v => /.+@.+/.test(v) || 'Email must be valid',
+      },
+      user: {
+        email: '',
+        password: '',
+      },
+    };
+  },
+};
+</script>
+
+<style scoped>
+.v-card {
+  border-top-left-radius: 1rem;
+  border-top-right-radius: 1rem;
+}
+</style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,14 +1,94 @@
 <template>
-  <LoginDesktop></LoginDesktop>
+  <component :is="login">
+    <template slot="form">
+      <v-form v-model="valid" class="pt-5">
+        <label  class="v-label"
+                :class="labelColour"
+        >
+          EMAIL
+        </label>
+        <v-text-field solo-inverted
+                      flat
+                      dark
+                      single-line
+                      color="#c22032"
+                      :rules="[rules.required, rules.email]"
+                      height="20px"
+                      v-model="user.email"
+        ></v-text-field>
+
+        <label  class="v-label"
+                :class="labelColour"
+        >
+          PASSWORD
+        </label>
+        <v-text-field solo-inverted
+                      flat
+                      dark
+                      single-line
+                      color="#c22032"
+                      :rules="[rules.required]"
+                      :append-icon="show ? 'mdi-eye-off' : 'mdi-eye'"
+                      :type="show ? 'text' : 'password'"
+                      @click:append="show = !show"
+                      height="20px"
+                      v-model="user.password"
+        ></v-text-field>
+        <v-flex class="text-xs-center">
+          <v-btn  large
+                  round
+                  :outline="$vuetify.breakpoint.smAndDown"
+                  :depressed="$vuetify.breakpoint.mdAndUp"
+                  :color="btnColour"
+                  class="mt-4 px-5"
+          >
+            Login
+          </v-btn>
+          <v-spacer/>
+          <v-btn  flat
+                  small
+                  round
+                  color="grey"
+          >
+            Forgot Password?
+          </v-btn>
+        </v-flex>
+      </v-form>
+    </template>
+  </component>
 </template>
 
 <script>
-import LoginDesktop from '@/components/LoginDesktop.vue';
+import mobile from '@/components/login/Mobile.vue';
+import desktop from '@/components/login/Desktop.vue';
 
 export default {
   name: 'login',
-  components: {
-    LoginDesktop,
+  title: 'Login',
+  data() {
+    return {
+      valid: false,
+      show: false,
+      rules: {
+        required: v => !!v || 'This field is required',
+        email: v => /.+@.+/.test(v) || 'Email must be valid',
+      },
+      user: {
+        email: '',
+        password: '',
+      },
+    };
+  },
+  computed: {
+    login() {
+      return this.$vuetify.breakpoint.smAndDown ? mobile : desktop;
+    },
+    btnColour() {
+      return this.$vuetify.breakpoint.smAndDown ? '#c22032' : '#e8e8e8';
+    },
+    labelColour() {
+      return this.$vuetify.breakpoint.smAndDown ? 'theme--light' : 'theme--dark';
+    },
   },
 };
 </script>
