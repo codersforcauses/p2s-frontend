06e8b48c16a280f10e8a63aa94d60f739d8010c6
Ben Locke <benlocke@outlook.com.au>
2018-12-28 09:24:30 +0800

    mobile remove bottom bar

--- src/components/login/Mobile.vue
+++ src/components/login/Mobile.vue
@@ -1,17 +1,16 @@
 <template>
   <v-content>
-    <v-toolbar app dark flat color="#c22032">
-      <v-toolbar-title>
-        Login
-      </v-toolbar-title>
-    </v-toolbar>
-
-    <v-container grid-list-lg fluid fill-height style="background-color: #c22032;">
+    <v-container fluid fill-height style="background-color: #c22032;">
       <v-layout fill-height row wrap>
-        <v-flex xs12>
-          <v-card color="#ebebeb" class="pa-4" style="margin: -16px" height="100%">
+        <v-flex style="height: 100%;" xs12>
+          <v-toolbar dark flat color="#c22032">
+           <v-toolbar-title>
+              Login
+            </v-toolbar-title>
+          </v-toolbar>
+          <v-card color="#ebebeb" class="pa-4" style="margin: 0px -16px" height="100%">
             <v-img  contain
-                    class="mx-auto py-5"
+                    class="mx-auto pt-5"
                     max-width="32vmax"
                     :src="require('@/assets/p2s_massive.png')"
             ></v-img>
@@ -22,25 +21,6 @@
     </v-container>
   </v-content>
 </template>
-
-<script>
-export default {
-  data() {
-    return {
-      valid: false,
-      show: false,
-      rules: {
-        required: v => !!v || 'This field is required',
-        email: v => /.+@.+/.test(v) || 'Email must be valid',
-      },
-      user: {
-        email: '',
-        password: '',
-      },
-    };
-  },
-};
-</script>
 
 <style scoped>
 .v-card {
