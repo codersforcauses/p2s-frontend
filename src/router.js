02e7d8c1ec9e8d4735d473e049b8143b697979d3
Jeremiah Pinto <jeremiahpinto279@outlook.com>
2019-01-02 17:24:06 +0800

    Added scroll behaviour check

--- src/router.js
+++ src/router.js
@@ -16,6 +16,9 @@ const isAuthenticated = (to, from, next) => {
 export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
+  scrollBehavior(to, from, savedPosition) {
+    return savedPosition || { x: 0, y: 0 };
+  },
   routes: [
     {
       path: '/login',
