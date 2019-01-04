c9d8497b644d8810873141d42f666ee07664ec12
Jeremiah Pinto <jeremiahpinto279@outlook.com>
2019-01-04 20:00:33 +0800

    Dark theme based on user preference and updates store accordingly

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app :dark="dark">
-    <toolbar @dark="dark = !dark;" v-bind="{ dark }" />
+    <toolbar @dark="setTheme" v-bind="{ dark }" />
     <v-content>
       <v-container fluid grid-list-lg>
         <v-layout row wrap>
@@ -13,16 +13,31 @@
 
 <script>
 import toolbar from '@/other/Toolbar.vue';
+import { mapActions } from 'vuex';
 
 export default {
   name: 'dashboard',
   components: {
     toolbar,
   },
+  mounted() {
+    this.dark = this.auth.user.darktheme;
+  },
   data() {
     return {
+      auth: this.$store.state.auth,
       dark: false,
     };
+  },
+  methods: {
+    ...mapActions('users', { patchTheme: 'patch' }),
+    setTheme() {
+      this.dark = !this.dark;
+      this.patchTheme([
+        this.auth.payload.userId,
+        { darktheme: this.dark },
+      ]);
+    },
   },
 };
 </script>
