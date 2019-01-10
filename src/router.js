0a74960585576fff4e7c2b8fe7aee05687b3c0ef
fezproof <stevebob1997@hotmail.com>
2019-01-10 15:42:33 +0800

    Added router guards that checks roles before enter

--- src/router.js
+++ src/router.js
@@ -9,15 +9,18 @@ const Login = () => import(/* webpackChunkName: "login" */ './views/Login.vue');
 const Dashboard = () => import(/* webpackChunkName: "dashboard" */ './views/Dashboard.vue');
 const Error = () => import(/* webpackChunkName: "404page" */ './views/404.vue');
 
+const Admin = () => import(/* webpackChunkName: "admin" */ './components/Admin/Index.vue');
+
 Vue.use(Router);
 
-const isAuthenticated = (to, from, next) => {
-  Store.dispatch('auth/authenticate')
-    .then(() => next())
-    .catch(() => next({ name: 'login' }));
-};
+// const isAuthenticated = (to, from, next) => {
+//   Store.dispatch('auth/authenticate')
+//     .then(() => next())
+//     .catch(() => next({ name: 'login' }));
+// };
 
-export default new Router({
+
+const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   scrollBehavior(to, from, savedPosition) {
@@ -36,17 +39,51 @@ export default new Router({
       beforeEnter(to, from, next) {
         Store.dispatch('auth/authenticate')
           .then(() => next({ name: 'dashboard' }))
-          .catch(() => next());
+          .catch(next);
       },
     },
     {
       path: '/',
       name: 'dashboard',
       component: Dashboard,
-      beforeEnter: isAuthenticated,
-      // children: [
-      //   {},
-      // ],
+      meta: { requiresAuth: true },
+      // beforeEnter: isAuthenticated,
+      children: [
+        {
+          path: 'admin',
+          name: 'admin',
+          component: Admin,
+          meta: { requiresAuth: true, permission: 'admin' },
+          // beforeEnter: isAuthenticated,
+        },
+      ],
     },
   ],
 });
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth === true)) {
+    // this route requires auth, check if logged in
+    // if not, redirect to login page.
+    Store.dispatch('auth/authenticate')
+      .then(() => {
+        const requiredPerm = to.matched.find(record => record.meta.permission);
+        if (requiredPerm !== undefined) {
+          if (Store.getters['users/current'][requiredPerm.meta.permission].is === true) {
+            next();
+          } else {
+            next({ name: 'error' });
+          }
+        } else {
+          next();
+        }
+      })
+      .catch(() => {
+        next({ name: 'login' });
+      });
+  } else {
+    next(); // make sure to always call next()!
+  }
+});
+
+export default router;
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -7,43 +7,7 @@
                 :color="sheet"
       >
         <v-container fluid grid-list-lg>
-          <v-layout fill-height row wrap>
-            <v-flex xs12 sm6 md4 v-for="i in 12" :key="i">
-              <v-card flat>
-                <v-sparkline
-                  :value="values"
-                  :color="primary"
-                  smooth="16"
-                  line-width="2"
-                  auto-draw
-                ></v-sparkline>
-
-                <v-card-title primary-title>
-                  <div>
-                    <h3 class="headline mb-0"
-                    :class="`${primary}--text`">Kangaroo Valley Safari</h3>
-                    <div>
-                      Located two hours south of Sydney in the
-                      <br>
-                      Southern Highlands of New South Wales, ...
-                    </div>
-                  </div>
-                </v-card-title>
-
-                <v-card-actions class="pa-0">
-                  <v-btn
-                    flat
-                    round
-                  >Share</v-btn>
-                  <v-btn
-                    flat
-                    round
-                  >Explore</v-btn>
-                </v-card-actions>
-              </v-card>
-            </v-flex>
-            <router-view/>
-          </v-layout>
+          <router-view :dark="dark"/>
         </v-container>
       </v-sheet>
     </v-content>
@@ -67,7 +31,6 @@ export default {
   data() {
     return {
       dark: false,
-      values: [0, 5, 2, 7, 8, 2, 7, 5, 3, 7, 9, 1, 7, 2, 4],
     };
   },
   mounted() {
