d785b4956ad7618c2c24ab55b105e69bc4156428
Jeremiah Pinto <jeremiahpinto@Jeremiahs-MacBook-Pro.local>
2019-01-22 03:20:23 +0800

    Fixed indentation

--- src/components/admin/ActivityCard.vue
+++ src/components/admin/ActivityCard.vue
@@ -2,9 +2,8 @@
   <v-card flat>
     <v-card-title primary-title>
       <div>
-        <h3
-          class="headline mb-0"
-          :class="`${primary}--text`"
+        <h3 class="headline mb-0"
+            :class="`${primary}--text`"
         >
           Manage Activties
         </h3>
@@ -17,23 +16,23 @@
     </v-card-title>
 
     <v-card-actions class="pa-0">
-      <v-btn
-        flat
-        round
-      >View All</v-btn>
-      <v-btn
-        flat
-        round
-      >Create New</v-btn>
+      <v-btn  flat
+              round
+      >
+        View All
+      </v-btn>
+      <v-btn  flat
+              round
+      >
+        Create New
+      </v-btn>
     </v-card-actions>
   </v-card>
 </template>
 
 <script>
 export default {
-  props: [
-    'primary',
-  ],
+  props: ['primary'],
 };
 </script>
 
