b48195dc1ee1e333f7a62fb17061a986e9dac6ce
Papajmil <21985993@student.uwa.edu.au>
2019-01-23 15:37:28 +0800

    added frontend button functionality
    when buttons are pressed they show the new text fields required to change the users info

--- src/components/UserSettings.vue
+++ src/components/UserSettings.vue
@@ -2,56 +2,112 @@
   <v-container grid-list-xs test-xs-center>
     <v-layout row wrap>
       <v-flex xs12>
-        <v-card-text>
-          Email:
-          <v-text-field
-          v-model="email"
-          :value=email
-          readonly=true
-          >
-          </v-text-field>
-          <v-btn
-          round
-          absolute
-          color="error"
-          >
-          Change Email</v-btn>
-        </v-card-text>
+        <v-card>
+          <v-card-text>
+            Email:
+            <v-text-field
+            v-model="email"
+            readonly="true"
+            >
+            </v-text-field>
+            <v-btn
+            round
+            color="error"
+            @click="changeEmail = !changeEmail"
+            >
+            Change Email</v-btn>
+            <v-card-text
+            v-show="changeEmail"
+            >
+              New Email:
+              <v-text-field
+              >
+              </v-text-field>
+              New Email Confirmation:
+              <v-text-field
+              >
+              </v-text-field>
+              <v-btn
+              round
+              color="error"
+              >
+              Update</v-btn>
+            </v-card-text>
+          </v-card-text>
+        </v-card>
       </v-flex>
       <v-flex xs12>
-        <v-card-text>
-          Username:
-          <v-text-field
-          v-model="username"
-          :value=username
-          readonly=true
-          >
-          </v-text-field>
-           <v-btn
-           round
-           absolute
-           color="error"
-           >
-           Change Username</v-btn>
-        </v-card-text>
+        <v-card>
+          <v-card-text>
+            Username:
+            <v-text-field
+            v-model="username"
+            :value=username
+            readonly=true
+            >
+            </v-text-field>
+            <v-btn
+            round
+            color="error"
+            @click="changeUsername = !changeUsername"
+            >
+            Change Username</v-btn>
+            <v-card-text
+            v-show="changeUsername"
+            >
+              New Username:
+              <v-text-field
+              >
+              </v-text-field>
+              New Username Confirmation:
+              <v-text-field
+              >
+              </v-text-field>
+              <v-btn
+              round
+              color="error"
+              >
+              Update</v-btn>
+            </v-card-text>
+          </v-card-text>
+        </v-card>
       </v-flex>
       <v-flex xs12>
-        <v-card-text>
-          Password:
-          <v-text-field
-          v-model="password"
-          :type="show1 ? 'text' : 'password'"
-          :value=password
-          readonly=true
-          >
-          </v-text-field>
-           <v-btn
-           round
-           absolute
-           color="error"
-           >
-           Change Password</v-btn>
-        </v-card-text>
+        <v-card>
+          <v-card-text>
+            Password:
+            <v-text-field
+            v-model="password"
+            :type="show1 ? 'text' : 'password'"
+            :value=password
+            readonly=true
+            >
+            </v-text-field>
+            <v-btn
+            round
+            color="error"
+            @click="changePassword = !changePassword"
+            >
+            Change Password</v-btn>
+            <v-card-text
+            v-show="changePassword"
+            >
+              New Password:
+              <v-text-field
+              >
+              </v-text-field>
+              New Password Confirmation:
+              <v-text-field
+              >
+              </v-text-field>
+              <v-btn
+              round
+              color="error"
+              >
+              Update</v-btn>
+            </v-card-text>
+          </v-card-text>
+        </v-card>
       </v-flex>
     </v-layout>
   </v-container>
@@ -64,6 +120,11 @@ export default {
       email: 'test@gmail.com',
       username: 'papa',
       password: '123',
+      changeEmail: false,
+      changeUsername: false,
+      changePassword: false,
+      rules: {
+      },
     };
   },
 };
