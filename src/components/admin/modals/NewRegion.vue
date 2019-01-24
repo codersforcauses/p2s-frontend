43e9a663f4ddcee79951640f61309697e4a40284
JeremiahPinto <jeremiahpinto279@outlook.com>
2019-01-24 17:50:22 +0800

    Removed copied code caused by copy pasta from staff invite

--- src/components/admin/modals/NewRegion.vue
+++ src/components/admin/modals/NewRegion.vue
@@ -18,7 +18,7 @@
         <v-form v-model="valid"
                 class="pt-2"
                 :class="{ 'px-2': $vuetify.breakpoint.smAndUp }"
-                @keyup.native.enter="valid && createUser($event) && !checkPerm && !!checkPerm"
+                @keyup.native.enter="valid && createRegion($event)"
         >
           <label class="v-label ml-4">
             NAME
