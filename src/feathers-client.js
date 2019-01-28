6ac05082ae5712492ef6d4dfa02b178f2cd3f87b
JeremiahPinto <jeremiahpinto279@outlook.com>
2019-01-28 19:37:26 +0800

    Added port variable for prod

--- src/feathers-client.js
+++ src/feathers-client.js
@@ -1,7 +1,8 @@
 import feathers from '@feathersjs/client';
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:3030', { transports: ['websocket'] });
+const port = process.env.NODE_ENV === 'production' ? 'http://localhost' : 'http://localhost:3030';
+const socket = io(port, { transports: ['websocket'] });
 
 const feathersClient = feathers()
   .configure(feathers.socketio(socket))
