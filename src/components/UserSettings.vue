b4f6d6a383d025b3d4151669cd9c420f39697ed5
Papajmil <21985993@student.uwa.edu.au>
2019-01-31 12:58:10 +0800

    update
    some basic rules and hints added

--- src/components/UserSettings.vue
+++ src/components/UserSettings.vue
@@ -24,46 +24,48 @@
               >
               </v-text-field>
             </v-flex>
-            <div
-            v-show="changeEmail"
-            >
-              <v-flex
-              xs12
-              tag="label"
-              class="v-label ml-4"
-              >
-                CHANGE EMAIL
-              </v-flex>
-              <div class="mt-2">
-                <v-text-field
-                v-model='newEmail'
-                class="ml-2 mr-2"
-                label="New Email"
-                single-line
-                solo-inverted
-                hide-details
-                flat
+            <v-expand-transition>
+              <div v-show="changeEmail">
+                <v-flex
+                xs12
+                tag="label"
+                class="v-label ml-4"
                 >
-                </v-text-field>
-                <v-text-field
-                v-model='newEmailCon'
-                class="ml-2 mr-2"
-                label="Confirm New Email"
-                single-line
-                solo-inverted
-                hide-details
-                flat
+                  CHANGE EMAIL
+                </v-flex>
+                <div class="mt-2">
+                  <v-text-field
+                  v-model='newEmail'
+                  class="ml-2 mr-2"
+                  hint="Enter new Email"
+                  persistent-hint
+                  single-line
+                  solo-inverted
+                  flat
+                  :rules="[validation.required, validation.email]"
+                  >
+                  </v-text-field>
+                  <v-text-field
+                  v-model='newEmailCon'
+                  class="ml-2 mr-2"
+                  hint="Confirm new Email"
+                  persistent-hint
+                  single-line
+                  solo-inverted
+                  flat
+                  :rules="[validation.required, validation.email]"
+                  >
+                  </v-text-field>
+                </div>
+                <v-btn
+                class="ml-4"
+                round
+                color="error"
+                depressed
                 >
-                </v-text-field>
+                Update</v-btn>
               </div>
-              <v-btn
-              class="ml-4"
-              round
-              color="error"
-              depressed
-              >
-              Update</v-btn>
-            </div>
+            </v-expand-transition>
           </v-card-text>
         </v-card>
       </v-flex>
@@ -90,53 +92,59 @@
               >
               </v-text-field>
             </v-flex>
-            <div
-            v-show="changeUsername"
-            >
-              <v-flex
-              xs12
-              tag="label"
-              class="v-label ml-4"
-              >
-                CHANGE USERNAME
-              </v-flex>
-              <div class="mt-2">
-                <v-text-field
-                v-model='newUsername'
-                class="ml-2 mr-2"
-                label="New Username"
-                single-line
-                solo-inverted
-                hide-details
-                flat
+            <v-expand-transition>
+              <div v-show="changeUsername">
+                <v-flex
+                xs12
+                tag="label"
+                class="v-label ml-4"
                 >
-                </v-text-field>
-                <v-text-field
-                v-model='newUsernameCon'
-                class="ml-2 mr-2"
-                label="Confirm New Username"
-                single-line
-                solo-inverted
-                hide-details
-                flat
+                  CHANGE USERNAME
+                </v-flex>
+                <div class="mt-2">
+                  <v-text-field
+                  v-model='newUsername'
+                  class="ml-2 mr-2"
+                  hint="Enter new Username"
+                  persistent-hint
+                  single-line
+                  solo-inverted
+                  flat
+                  >
+                  </v-text-field>
+                  <v-text-field
+                  v-model='newUsernameCon'
+                  class="ml-2 mr-2"
+                  hint="Confirm new Username"
+                  persistent-hint
+                  single-line
+                  solo-inverted
+                  flat
+                  >
+                  </v-text-field>
+                </div>
+                <v-btn
+                class="ml-4"
+                round
+                color="error"
+                depressed
                 >
-                </v-text-field>
+                Update</v-btn>
               </div>
-              <v-btn
-              class="ml-4"
-              round
-              color="error"
-              depressed
-              >
-              Update</v-btn>
-            </div>
+            </v-expand-transition>
           </v-card-text>
         </v-card>
       </v-flex>
       <v-flex xs12>
         <v-card>
           <v-card-text>
-            PASSWORD
+            <v-flex
+              xs12
+              tag="label"
+              class="v-label ml-4"
+              >
+              PASSWORD
+            </v-flex>
             <v-flex>
               <v-text-field
               v-model="username"
@@ -151,46 +159,50 @@
               >
               </v-text-field>
             </v-flex>
-            <div
-            v-show="changePassword"
-            >
-              <v-flex
-              xs12
-              tag="label"
-              class="v-label ml-4"
-              >
-                CHANGE PASSWORD
-              </v-flex>
-              <div class="mt-2">
-                <v-text-field
-                v-model='newPassword'
-                class="ml-2 mr-2"
-                label="New Password"
-                single-line
-                solo-inverted
-                hide-details
-                flat
+            <v-expand-transition>
+              <div v-show="changePassword">
+                <v-flex
+                xs12
+                tag="label"
+                class="v-label ml-4"
                 >
-                </v-text-field>
-                <v-text-field
-                v-model='newPasswordCon'
-                class="ml-2 mr-2"
-                label="Confirm New Password"
-                single-line
-                solo-inverted
-                hide-details
-                flat
+                  CHANGE PASSWORD
+                </v-flex>
+                <div class="mt-2">
+                  <v-text-field
+                  v-model='newPassword'
+                  class="ml-2 mr-2"
+                  hint="Enter new Password"
+                  type="password"
+                  persistent-hint
+                  single-line
+                  solo-inverted
+                  flat
+                  :rules=[validation.required]
+                  >
+                  </v-text-field>
+                  <v-text-field
+                  v-model='newPasswordCon'
+                  class="ml-2 mr-2"
+                  hint="Confirm new Password"
+                  type="password"
+                  persistent-hint
+                  single-line
+                  solo-inverted
+                  flat
+                  :rules=[validation.required]
+                  >
+                  </v-text-field>
+                </div>
+                <v-btn
+                class="ml-4"
+                round
+                color="error"
+                depressed
                 >
-                </v-text-field>
+                Update</v-btn>
               </div>
-              <v-btn
-              class="ml-4"
-              round
-              color="error"
-              depressed
-              >
-              Update</v-btn>
-            </div>
+            </v-expand-transition>
           </v-card-text>
         </v-card>
       </v-flex>
@@ -212,22 +224,12 @@ export default {
       newEmail: '',
       newEmailCon: '',
       emailUpdate: false,
-      rules: {
+      validation: {
         required: value => !!value || 'Required',
-        isEmail: value => value.includes('@') || 'Not an email',
-        emailMatch: (newEmail, newEmailCon) => newEmail === newEmailCon || 'Not the same',
-      },
-      computed: {
-        /*  newEmail: function () {
-          if (this.newEmail === this.newEmailCon)
-          {
-            this.emailUpdate = true;
-          }
-          else
-          {
-            this.emailUpdate = false;
-          }
-        },  */
+        email: (value) => {
+          const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+          return pattern.test(value) || 'Invalid email address';
+        },
       },
     };
   },
