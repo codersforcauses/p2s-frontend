99419eaf4d64bf11b350653a6b75dc37b78242bf
JeremiahPinto <jeremiahpinto279@outlook.com>
2019-02-01 13:50:24 +0800

    Added coach dashboard

--- src/components/coach/Index.vue
+++ src/components/coach/Index.vue
@@ -1,66 +1,36 @@
 <template>
   <v-layout fill-height row wrap>
-    <v-flex xs12 sm6 md4 v-for="i in 12" :key="i">
-      <v-card flat>
-        <v-sparkline
-          :value="values"
-          :color="primary"
-          smooth="16"
-          line-width="2"
-          auto-draw
-        ></v-sparkline>
-
-        <v-card-title primary-title>
-          <div>
-            <h3 class="headline mb-0"
-            :class="`${primary}--text`">Kangaroo Valley Safari</h3>
-            <div>
-              Located two hours south of Sydney in the
-              <br>
-              Southern Highlands of New South Wales, ...
-            </div>
-          </div>
-        </v-card-title>
-
-        <v-card-actions class="pa-0">
-          <v-btn
-            flat
-            round
-          >Share</v-btn>
-          <v-btn
-            flat
-            round
-            @click="dialog = true"
-          >Explore</v-btn>
-        </v-card-actions>
-      </v-card>
+    <v-flex xs12 sm6 md4
+            v-for="(card, index) in cards"
+            :key="index"
+    >
+      <component :is="card" :primary="primary"/>
     </v-flex>
-    <invite v-model="dialog" v-bind="{ dark }"/>
   </v-layout>
 </template>
 
 <script>
-const invite = import('@/components/admin/modals/UserInvite.vue');
+import spinner from '@/other/Spinner.vue';
 
 export default {
   title: 'Coach Dashboard',
+  components: {
+    'student-card': () => ({
+      component: import('./StudentCard.vue'),
+      loading: spinner,
+    }),
+  },
   props: {
     dark: {
       type: Boolean,
       default: false,
     },
   },
-  components: {
-    invite: () => ({
-      component: invite,
-      delay: 100,
-      setTimeout: 3000,
-    }),
-  },
   data() {
     return {
-      values: [0, 5, 2, 7, 8, 2, 7, 5, 3, 7, 9, 1, 7, 2, 4],
-      dialog: false,
+      cards: [
+        'student-card',
+      ],
     };
   },
   computed: {
@@ -70,7 +40,3 @@ export default {
   },
 };
 </script>
-
-<style>
-
-</style>
