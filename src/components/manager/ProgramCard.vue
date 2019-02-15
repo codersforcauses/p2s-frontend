55048474a4fb5ba118592a796a62320aeda8ad40
JeremiahPinto <jeremiahpinto279@outlook.com>
2019-02-15 15:40:33 +0800

    Added program card

---
+++ src/components/manager/ProgramCard.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-card flat>
+    <v-card-title primary-title>
+      <div>
+        <h3 class="headline mb-0"
+            :class="`${primary}--text`"
+        >
+          View Programs
+        </h3>
+        <div>
+          View all programs in your region
+        </div>
+      </div>
+    </v-card-title>
+
+    <v-card-actions class="pa-0">
+      <v-btn  flat
+              round
+      >
+        View All
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: ['primary'],
+};
+</script>
+
+<style scoped>
+</style>
