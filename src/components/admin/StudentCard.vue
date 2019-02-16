40971f80c6936ae0b0ed98cdc21cf37bfaa4be16
JeremiahPinto <jeremiahpinto279@outlook.com>
2019-02-16 16:49:19 +0800

    Added in school, session and student cards for admin dashboard

---
+++ src/components/admin/StudentCard.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-card flat>
+    <v-card-title primary-title>
+      <div>
+        <h3 class="headline mb-0"
+            :class="`${primary}--text`"
+        >
+          Manage Students
+        </h3>
+        <div>
+          View all the students on record
+          <!-- <br>
+          Create a new activity to be used in lessons or programs -->
+        </div>
+      </div>
+    </v-card-title>
+
+    <v-card-actions class="pa-0">
+      <v-btn  flat
+              round
+      >
+        View All
+      </v-btn>
+      <v-btn  flat
+              round
+      >
+        Create New
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: ['primary'],
+};
+</script>
+
+<style scoped>
+</style>
