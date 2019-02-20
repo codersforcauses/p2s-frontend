db26d03a83b58bd2788b99a2a8682a320d98c692
JeremiahPinto <jeremiahpinto279@outlook.com>
2019-02-20 21:16:27 +0800

    Modified Index.vue to use child components for all coach pages

--- src/components/coach/Index.vue
+++ src/components/coach/Index.vue
@@ -1,47 +1,9 @@
 <template>
-  <v-layout fill-height row wrap>
-    <v-flex xs12 sm6 md4
-            v-for="(card, index) in cards"
-            :key="index"
-    >
-      <component :is="card" :primary="primary"/>
-    </v-flex>
-  </v-layout>
+  <router-view v-bind="{ dark }" />
 </template>
 
 <script>
-import spinner from '@/other/Spinner.vue';
-
 export default {
-  title: 'Coach Dashboard',
-  components: {
-    'student-card': () => ({
-      component: import('./StudentCard.vue'),
-      loading: spinner,
-    }),
-    'session-card': () => ({
-      component: import('./SessionCard.vue'),
-      loading: spinner,
-    }),
-  },
-  props: {
-    dark: {
-      type: Boolean,
-      default: false,
-    },
-  },
-  data() {
-    return {
-      cards: [
-        'student-card',
-        'session-card',
-      ],
-    };
-  },
-  computed: {
-    primary() {
-      return this.dark ? 'darkPrimary' : 'lightPrimary';
-    },
-  },
+  props: ['dark'],
 };
 </script>
