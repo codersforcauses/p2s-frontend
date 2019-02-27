e61e3e18a6872cf812d0c4fb5420b477474fd51a
JeremiahPinto <jeremiahpinto279@outlook.com>
2019-02-28 05:01:01 +0800

    Added student report modal

--- src/components/coach/modals/StudentReport.vue
+++ src/components/coach/modals/StudentReport.vue
@@ -7,7 +7,7 @@
   >
     <v-card flat tile>
       <v-toolbar flat>
-        <h1 class="headline"> {{ session.name }} </h1>
+        <h1 class="headline"> {{ heading }} </h1>
         <v-spacer/>
         <v-btn flat icon @click="$emit('input')">
           <v-icon> mdi-close </v-icon>
@@ -22,84 +22,41 @@
         >
           <v-layout row wrap>
             <v-flex xs12 tag="label" class="v-label ml-4">
-              ACTIVITIES PLANNED
-            </v-flex>
-            <v-flex xs12 style="padding-right: 1px;">
-              <v-textarea solo-inverted
-                          flat
-                          persistent-hint
-                          auto-grow
-                          counter=""
-                          type="text"
-                          class="mb-2 mt-1"
-                          placeholder="Enter details in your own words on how the session went"
-                          :color="primary"
-                          :rules="[validation.required, validation.name]"
-              >
-              </v-textarea>
-            </v-flex>
-
-            <v-flex xs12 tag="label" class="v-label ml-4">
-              STUDENT REPORT AND ATTENDANCE
+              STUDENT ASSESSMENT
             </v-flex>
             <v-flex xs12>
-              <v-list two-line class="mt-1 pb-0">
-                <v-list-tile
-                  v-for="student in incompletedStudents"
-                  :key="student._id"
-                  @click="temp = true"
-                >
-                  <v-list-tile-content>
-                    <v-list-tile-title>
-                      {{ `${student.name.first} ${student.name.last}` }}
-                    </v-list-tile-title>
-                  </v-list-tile-content>
-
-                  <v-list-tile-avatar>
-                    <v-checkbox v-model="temp" :color="primary"></v-checkbox>
-                  </v-list-tile-avatar>
-                </v-list-tile>
-              </v-list>
-              <v-expansion-panel focusable class="mb-4">
-                <v-expansion-panel-content>
-                  <div slot="header">
-                    Completed
-                  </div>
-                  <v-list class="mt-1">
-                    <v-list-tile
-                      v-for="student in completedStudents"
-                      :key="student._id"
-                      @click="temp = true"
-                    >
-                      <v-list-tile-content>
-                        <v-list-tile-title>
-                          {{ `${student.name.first} ${student.name.last}` }}
-                        </v-list-tile-title>
-                      </v-list-tile-content>
-
-                      <v-list-tile-avatar>
-                        <v-checkbox v-model="temp" :color="primary"></v-checkbox>
-                      </v-list-tile-avatar>
-                    </v-list-tile>
-                  </v-list>
-                </v-expansion-panel-content>
-              </v-expansion-panel>
+              <v-data-table :headers="headers"
+                            :items="items"
+                            hide-actions
+                            item-key="id"
+                            loading="true"
+              >
+                <template slot="items" slot-scope="props">
+                  <td>{{ props.item.name }}</td>
+                  <td class="text-xs-right">{{ props.item.calories }}</td>
+                  <td class="text-xs-right">{{ props.item.fat }}</td>
+                  <td class="text-xs-right">{{ props.item.carbs }}</td>
+                  <td class="text-xs-right">{{ props.item.protein }}</td>
+                  <td class="text-xs-right">{{ props.item.iron }}</td>
+                </template>
+              </v-data-table>
             </v-flex>
 
             <v-flex xs12 tag="label" class="v-label ml-4">
-              SESSION FEEDBACK
+              COMMENTS
             </v-flex>
-            <v-flex xs12 style="padding-right: 1px;">
+            <v-flex xs12 class="mt-1">
               <v-textarea solo-inverted
                           flat
                           persistent-hint
                           auto-grow
-                          counter=""
+                          counter="250"
+                          maxlength="250"
                           type="text"
                           class="mb-2 mt-1"
-                          placeholder="Enter details in your own words on how the session went"
+                          placeholder="Enter your comments on the student during this session"
                           :color="primary"
-                          :rules="[validation.required, validation.name]"
+                          :rules="[validation.required]"
               >
               </v-textarea>
             </v-flex>
@@ -127,7 +84,7 @@
                       :disabled="!valid"
                       @click.stop.prevent="createSession"
               >
-                <span :style="{ color: button }"> Send Invite </span>
+                <span :style="{ color: button }"> Submit Report </span>
               </v-btn>
             </v-flex>
           </v-layout>
@@ -141,7 +98,7 @@
 import { mapState, mapGetters, mapActions } from 'vuex';
 
 export default {
-  props: ['value', 'dark', 'sessionId'],
+  props: ['value', 'dark', 'student'],
   data() {
     return {
       temp: false,
@@ -152,6 +109,129 @@ export default {
       error: '',
       valid: false,
       finished: false,
+      headers: [{
+        text: '',
+        align: 'left',
+        sortable: false,
+        // value: matrix.category.matrix.name,
+      }, {
+        text: 'Level 1',
+        align: 'left',
+        sortable: false,
+        value: 1,
+      }, {
+        text: 'Level 2',
+        align: 'left',
+        sortable: false,
+        value: 2,
+      }, {
+        text: 'Level 3',
+        align: 'left',
+        sortable: false,
+        value: 3,
+      }, {
+        text: 'Level 4',
+        align: 'left',
+        sortable: false,
+        value: 4,
+      }, {
+        text: 'Level 5',
+        align: 'left',
+        sortable: false,
+        value: 5,
+      }, {
+        text: 'Level 6',
+        align: 'left',
+        sortable: false,
+        value: 6,
+      }, {
+        text: 'Level 7',
+        align: 'left',
+        sortable: false,
+        value: 7,
+      }],
+      items: [
+        {
+          name: 'Frozen Yogurt',
+          calories: 159,
+          fat: 6.0,
+          carbs: 24,
+          protein: 4.0,
+          iron: '1%',
+        },
+        {
+          name: 'Ice cream sandwich',
+          calories: 237,
+          fat: 9.0,
+          carbs: 37,
+          protein: 4.3,
+          iron: '1%',
+        },
+        {
+          name: 'Eclair',
+          calories: 262,
+          fat: 16.0,
+          carbs: 23,
+          protein: 6.0,
+          iron: '7%',
+        },
+        {
+          name: 'Cupcake',
+          calories: 305,
+          fat: 3.7,
+          carbs: 67,
+          protein: 4.3,
+          iron: '8%',
+        },
+        {
+          name: 'Gingerbread',
+          calories: 356,
+          fat: 16.0,
+          carbs: 49,
+          protein: 3.9,
+          iron: '16%',
+        },
+        {
+          name: 'Jelly bean',
+          calories: 375,
+          fat: 0.0,
+          carbs: 94,
+          protein: 0.0,
+          iron: '0%',
+        },
+        {
+          name: 'Lollipop',
+          calories: 392,
+          fat: 0.2,
+          carbs: 98,
+          protein: 0,
+          iron: '2%',
+        },
+        {
+          name: 'Honeycomb',
+          calories: 408,
+          fat: 3.2,
+          carbs: 87,
+          protein: 6.5,
+          iron: '45%',
+        },
+        {
+          name: 'Donut',
+          calories: 452,
+          fat: 25.0,
+          carbs: 51,
+          protein: 4.9,
+          iron: '22%',
+        },
+        {
+          name: 'KitKat',
+          calories: 518,
+          fat: 26.0,
+          carbs: 65,
+          protein: 7,
+          iron: '6%',
+        },
+      ],
       validation: {
         required: value => !!value || 'This field is required',
         email: (value) => {
@@ -165,26 +245,11 @@ export default {
       },
     };
   },
-  mounted() {
-    this.findStudents({
-      query: {
-        id: this.sessionId,
-        $sort: {
-          'name.first': 1,
-          'name.last': 1,
-        },
-        $select: [
-          'name',
-          'schoolYear',
-          'gender',
-          'trial',
-        ],
-      },
-    }).then(this.finished = true);
-  },
   computed: {
-    ...mapState('students', { loadStudents: 'isFindPending' }),
-    ...mapGetters('students', { findStudentsInStore: 'find' }),
+    ...mapState('Reports', { loadReports: 'isFindPending' }),
+    ...mapGetters('Reports', { findReportsInStore: 'find' }),
+    ...mapState('Matrix', { loadMatrix: 'isFindPending' }),
+    ...mapGetters('Matrix', { findMatrixInStore: 'find' }),
     showDialog: {
       get() {
         return this.value;
@@ -199,35 +264,13 @@ export default {
     button() {
       return this.dark ? '#272727' : '#ebebeb';
     },
-    students() {
-      if (this.finished) {
-        const students = this.findStudentsInStore({
-          query: {
-            $limit: 15,
-            $sort: {
-              'name.first': 1,
-              'name.last': 1,
-            },
-          },
-        }).data;
-
-        const studentsInSession = students.map(student => ({
-          ...student,
-          completion: false,
-        }));
-        return studentsInSession;
-      }
-      return [];
-    },
-    incompletedStudents() {
-      return this.students.filter(student => !student.completion);
-    },
-    completedStudents() {
-      return this.students.filter(student => student.completion);
+    heading() {
+      return `${this.student.name.first}'s Report`;
     },
   },
   methods: {
-    ...mapActions('students', { findStudents: 'find' }),
+    ...mapActions('Reports', { findReports: 'find' }),
+    ...mapActions('Matrix', { findMatrix: 'find' }),
     createSession() {},
   },
 };
