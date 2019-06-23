4396f89a154cce4c1652143e0d721303dfe6629b
JeremiahPinto <jeremiahpinto279@outlook.com>
2019-06-24 01:16:07 +0800

    Added toolbar to registration form

---
+++ src/components/registration/Toolbar.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-toolbar flat app height=64>
+    <v-toolbar-title class="mt-2 ml-2 headline">
+      User Registration
+    </v-toolbar-title>
+
+    <v-spacer/>
+
+    <v-toolbar-items>
+      <v-btn  :flat="$vuetify.breakpoint.mdAndUp"
+              :icon="$vuetify.breakpoint.smAndDown"
+              @click="setTheme"
+      >
+        <v-icon :left="$vuetify.breakpoint.mdAndUp">
+          mdi-theme-light-dark
+        </v-icon>
+
+        <span v-show="$vuetify.breakpoint.mdAndUp">
+          {{ theme }} Theme
+        </span>
+      </v-btn>
+
+      <v-btn  disabled
+              :flat="$vuetify.breakpoint.mdAndUp"
+              :icon="$vuetify.breakpoint.smAndDown"
+      >
+        <span v-show="$vuetify.breakpoint.mdAndUp">
+          Logout
+        </span>
+
+        <v-icon :right="$vuetify.breakpoint.mdAndUp">
+          mdi-logout
+        </v-icon>
+      </v-btn>
+    </v-toolbar-items>
+  </v-toolbar>
+</template>
+
+<script>
+export default {
+  props: ['value'],
+  computed: {
+    dark: {
+      get() {
+        return this.value;
+      },
+      set(value) {
+        this.$emit('input', value);
+      },
+    },
+    theme() {
+      return this.dark ? 'Light' : 'Dark';
+    },
+  },
+  methods: {
+    async setTheme() {
+      this.dark = !this.dark;
+    },
+  },
+};
+</script>
