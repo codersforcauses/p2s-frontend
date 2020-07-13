f01eee6f9e07c5ef3f5b1f57ff4f3d0825ab1cb4
Drewbi <drewalexander986@gmail.com>
2020-07-13 14:20:36 +0800

    Add region separate page

---
+++ src/components/Region.vue
@@ -0,0 +1,30 @@
+<template>
+<v-layout row wrap>
+  <v-card>
+    <v-card-title>
+      Test
+    </v-card-title>
+    <v-list>
+      <v-list-tile avatar @click.native="regionSelected = true">
+        helllo
+      </v-list-tile>
+    </v-list>
+  </v-card>
+  <v-navigation-drawer
+      v-model="regionSelected"
+      absolute
+      right
+      width="1000px"
+    >
+      <v-card>Hi</v-card>
+    </v-navigation-drawer>
+    </v-layout>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    regionSelected: false,
+  }),
+};
+</script>
