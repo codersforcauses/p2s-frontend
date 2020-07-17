5f30ed6384c3724f9e1f05c39fc8ad7ad5b3ee4e
Chamika Kariyawasam <chamikakari@gmail.com>
2020-07-17 21:12:53 +0800

    correct querying of users and school for regions

--- src/components/Region.vue
+++ src/components/Region.vue
@@ -1,30 +1,75 @@
 <template>
-<v-layout row wrap>
   <v-card>
-    <v-card-title>
-      Test
-    </v-card-title>
-    <v-list>
-      <v-list-tile avatar @click.native="regionSelected = true">
-        helllo
-      </v-list-tile>
-    </v-list>
-  </v-card>
-  <v-navigation-drawer
-      v-model="regionSelected"
-      absolute
-      right
-      width="1000px"
-    >
-      <v-card>Hi</v-card>
-    </v-navigation-drawer>
+    <v-layout row wrap>
+      <v-flex xs12 md4>
+        <v-list v-for="region in oneRegion" :key="region">
+          <v-list-tile>Region name: <b>{{ `${region.name}` }}</b></v-list-tile>
+          <v-list-item-content>
+            <b>Schools</b>
+          <ul v-for="school in oneSchools" :key="school">
+            <li>{{school.name}}</li>
+          </ul>
+          <b>Users</b>
+          <ul v-for="user in oneUsers" :key='user'>
+            <li>{{user.name.first}},{{user.name.last}}</li>
+          </ul>
+          </v-list-item-content>
+        </v-list>
+      </v-flex>
     </v-layout>
+  </v-card>
 </template>
 
 <script>
+import { mapGetters, mapActions } from 'vuex';
+
 export default {
   data: () => ({
-    regionSelected: false,
+    oneRegion: {},
+    oneSchools: {},
+    oneUsers: {},
   }),
+  computed: {
+    ...mapGetters('regions', { listRegions: 'list' }),
+  },
+  methods: {
+    ...mapActions('regions', { findRegions: 'find' }),
+    ...mapActions('users', { findUsers: 'find' }),
+    ...mapActions('schools', { findSchools: 'find' }),
+  },
+  created() {
+    this.findRegions({
+      query: {
+        $limit: 1,
+      },
+    }).then((response) => {
+      this.oneRegion = response.data;
+    });
+    this.findSchools({
+      query: {
+        _id: {
+          $in: ['5f019ce2f0cce90a929ce235', '5f019ce2f0cce90a929ce236', '5f019ce2f0cce90a929ce238'],
+        },
+      },
+    }).then((response) => {
+      this.oneSchools = response.data;
+    });
+    this.findUsers({
+      query: {
+        _id: {
+          $in: ['5f019d01f0cce90a929ce244', '5f019d04f0cce90a929ce248', '5f019d04f0cce90a929ce245',
+            '5f019d04f0cce90a929ce246', '5f019d04f0cce90a929ce247', '5f019d04f0cce90a929ce249',
+            '5f019d04f0cce90a929ce24a'],
+        },
+      },
+    }).then((response) => {
+      this.oneUsers = response.data;
+    });
+  },
 };
 </script>
+<style scoped>
+ul{
+list-style: disc;
+}
+</style>
