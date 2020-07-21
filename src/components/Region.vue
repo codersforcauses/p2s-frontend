1bd553cea5a40078eeb91a9125e66711e2a50fb2
Chamika Kariyawasam <chamikakari@gmail.com>
2020-07-21 23:02:50 +0800

    final changes made using icons

--- src/components/Region.vue
+++ src/components/Region.vue
@@ -1,34 +1,64 @@
 <template>
-  <v-card>
-    <v-layout row wrap>
-      <v-flex xs12 md4>
-        <v-list v-for="region in oneRegion" :key="region">
-          <v-list-tile>Region name: <b>{{ `${region.name}` }}</b></v-list-tile>
-          <v-list-item-content>
-            <b>Schools</b>
-          <ul v-for="school in oneSchools" :key="school">
-            <li>{{school.name}}</li>
-          </ul>
-          <b>Users</b>
-          <ul v-for="user in oneUsers" :key='user'>
-            <li>{{user.name.first}},{{user.name.last}}</li>
-          </ul>
-          </v-list-item-content>
+  <v-layout row wrap>
+    <v-flex xs12 md4>
+      <v-card flat>
+        <v-list-item two-line>
+          <h2>Select a Region</h2>
+          <v-list-tile
+            v-for="region in allRegions"
+            :key="region._id"
+            @click="getRegion(region._id)"
+          >
+            {{ region.name }}
+          </v-list-tile>
+        </v-list-item>
+      </v-card>
+    </v-flex>
+    <v-flex xs12 md8>
+      <v-card flat>
+        <v-list>
+          <h3>Staff</h3>
+            <v-row xs6 md4 align="stretch" justify="space-around">
+              <h4>Key</h4>
+              <p>Coach <v-icon>mdi-account-tie</v-icon>
+              </p><p>Manager <v-icon>mdi-account-tie-outline</v-icon></p>
+              <p>Admin <v-icon>mdi-account-tie-voice</v-icon></p>
+            </v-row>
+          <v-list-tile v-for="user in users" :key="user._id">
+            <v-list-tile-content>
+              {{ `${user.name.first} ${user.name.last}` }}
+            </v-list-tile-content>
+            <v-icon v-if="user.coach.is">mdi-account-tie </v-icon>
+            <v-icon v-if="user.manager.is">mdi-account-tie-outline </v-icon>
+            <v-icon v-if="user.admin.is">mdi-account-tie-voice </v-icon>
+          </v-list-tile>
         </v-list>
-      </v-flex>
-    </v-layout>
-  </v-card>
+        <v-list>
+          <br />
+          <h3>Schools</h3>
+          <v-list-tile v-for="school in schools" :key="school._id">
+            {{ `${school.name}` }}
+            <v-list-content>{{school.phoneNumber}}</v-list-content> <br>
+          </v-list-tile>
+        </v-list>
+      </v-card>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
+/* eslint no-underscore-dangle: ["error", { "allow": ["_id"] }] */
 import { mapGetters, mapActions } from 'vuex';
 
 export default {
-  data: () => ({
-    oneRegion: {},
-    oneSchools: {},
-    oneUsers: {},
-  }),
+  data() {
+    return {
+      allRegions: [],
+      currentlySelectedRegion: [],
+      users: [],
+      schools: [],
+    };
+  },
   computed: {
     ...mapGetters('regions', { listRegions: 'list' }),
   },
@@ -36,40 +66,40 @@ export default {
     ...mapActions('regions', { findRegions: 'find' }),
     ...mapActions('users', { findUsers: 'find' }),
     ...mapActions('schools', { findSchools: 'find' }),
+    /* This method fetches the respective region given the id input in the click method */
+    getRegion(id) {
+      const selectedRegion = this.listRegions.find(regions => regions._id === id);
+      this.currentlySelectedRegion = selectedRegion;
+      this.findUsers({
+        query: {
+          _id: {
+            $in: this.currentlySelectedRegion.users,
+          },
+        },
+      }).then((response) => {
+        this.users = response.data;
+      });
+      this.findSchools({
+        query: {
+          _id: {
+            $in: this.currentlySelectedRegion.schools,
+          },
+        },
+      }).then((response) => {
+        this.schools = response.data;
+      });
+    },
   },
+  /* intialling fetching all the regions as a list when the page is created. Once
+  the regions have been loaded, then the user can individually click on them */
   created() {
     this.findRegions({
-      query: {
-        $limit: 1,
-      },
+      query: {},
     }).then((response) => {
-      this.oneRegion = response.data;
-    });
-    this.findSchools({
-      query: {
-        _id: {
-          $in: ['5f019ce2f0cce90a929ce235', '5f019ce2f0cce90a929ce236', '5f019ce2f0cce90a929ce238'],
-        },
-      },
-    }).then((response) => {
-      this.oneSchools = response.data;
-    });
-    this.findUsers({
-      query: {
-        _id: {
-          $in: ['5f019d01f0cce90a929ce244', '5f019d04f0cce90a929ce248', '5f019d04f0cce90a929ce245',
-            '5f019d04f0cce90a929ce246', '5f019d04f0cce90a929ce247', '5f019d04f0cce90a929ce249',
-            '5f019d04f0cce90a929ce24a'],
-        },
-      },
-    }).then((response) => {
-      this.oneUsers = response.data;
+      this.allRegions = response.data;
     });
   },
 };
 </script>
-<style scoped>
-ul{
-list-style: disc;
-}
-</style>
+
+<style></style>
