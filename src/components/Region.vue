1ecdf8eb1050a7a9b60f30aba69a623ef280ef86
Chamika Kariyawasam <chamikakari@gmail.com>
2020-07-22 15:09:21 +0800

    final brushing up with drew

--- src/components/Region.vue
+++ src/components/Region.vue
@@ -1,9 +1,14 @@
 <template>
   <v-layout row wrap>
     <v-flex xs12 md4>
-      <v-card flat>
-        <v-list-item two-line>
-          <h2>Select a Region</h2>
+      <v-card>
+        <v-card-title class="pb-0">
+          <h3 class="headline mb-0"
+            :class="`${primary}--text`">
+            Select a Region
+          </h3>
+        </v-card-title>
+        <v-list>
           <v-list-tile
             v-for="region in allRegions"
             :key="region._id"
@@ -11,34 +16,51 @@
           >
             {{ region.name }}
           </v-list-tile>
-        </v-list-item>
+        </v-list>
       </v-card>
     </v-flex>
-    <v-flex xs12 md8>
-      <v-card flat>
+    <v-flex xs12 md8 v-show="currentlySelectedRegion">
+      <v-card>
+        <v-card-title class="pb-0">
+          {{ !currentlySelectedRegion ? "No region selected" : currentlySelectedRegion.name }}
+        </v-card-title>
         <v-list>
-          <h3>Staff</h3>
-            <v-row xs6 md4 align="stretch" justify="space-around">
-              <h4>Key</h4>
-              <p>Coach <v-icon>mdi-account-tie</v-icon>
-              </p><p>Manager <v-icon>mdi-account-tie-outline</v-icon></p>
-              <p>Admin <v-icon>mdi-account-tie-voice</v-icon></p>
-            </v-row>
+          <v-subheader>Users</v-subheader>
           <v-list-tile v-for="user in users" :key="user._id">
             <v-list-tile-content>
               {{ `${user.name.first} ${user.name.last}` }}
             </v-list-tile-content>
-            <v-icon v-if="user.coach.is">mdi-account-tie </v-icon>
-            <v-icon v-if="user.manager.is">mdi-account-tie-outline </v-icon>
-            <v-icon v-if="user.admin.is">mdi-account-tie-voice </v-icon>
+            <v-tooltip bottom transition="none">
+              <template v-slot:activator="{ on }">
+               <v-icon v-if="user.coach.is" v-on="on">mdi-football-australian </v-icon>
+              </template>
+              <span>Coach</span>
+            </v-tooltip>
+            <v-tooltip bottom transition="none">
+              <template v-slot:activator="{ on }">
+               <v-icon v-if="user.manager.is" v-on="on">mdi-account-tie </v-icon>
+              </template>
+              <span>Manager</span>
+            </v-tooltip>
+            <v-tooltip bottom transition="none">
+              <template v-slot:activator="{ on }">
+               <v-icon v-if="user.admin.is" v-on="on">mdi-shield-account </v-icon>
+              </template>
+              <span>Admin</span>
+            </v-tooltip>
           </v-list-tile>
         </v-list>
         <v-list>
-          <br />
-          <h3>Schools</h3>
+          <v-subheader>Schools</v-subheader>
           <v-list-tile v-for="school in schools" :key="school._id">
-            {{ `${school.name}` }}
-            <v-list-content>{{school.phoneNumber}}</v-list-content> <br>
+            <v-list-tile-content>
+              {{ school.name }}
+            </v-list-tile-content>
+            <v-list-tile-action>
+              <v-subheader>
+                {{ school.phoneNumber }}
+              </v-subheader>
+            </v-list-tile-action>
           </v-list-tile>
         </v-list>
       </v-card>
@@ -54,52 +76,59 @@ export default {
   data() {
     return {
       allRegions: [],
-      currentlySelectedRegion: [],
+      currentlySelectedRegion: null,
       users: [],
       schools: [],
+      dark: false,
     };
   },
   computed: {
     ...mapGetters('regions', { listRegions: 'list' }),
+    primary() {
+      return this.$store.state.auth.user.darktheme ? 'darkPrimary' : 'lightPrimary';
+    },
   },
   methods: {
     ...mapActions('regions', { findRegions: 'find' }),
     ...mapActions('users', { findUsers: 'find' }),
     ...mapActions('schools', { findSchools: 'find' }),
+    populateUsers(region) {
+      return this.findUsers({
+        query: {
+          _id: {
+            $in: region.users,
+          },
+        },
+      });
+    },
+    populateSchools(region) {
+      return this.findSchools({
+        query: {
+          _id: {
+            $in: region.schools,
+          },
+        },
+      });
+    },
     /* This method fetches the respective region given the id input in the click method */
-    getRegion(id) {
-      const selectedRegion = this.listRegions.find(regions => regions._id === id);
+    async getRegion(id) {
+      const selectedRegion = this.allRegions.find(region => region._id === id);
+      const usersPromise = this.populateUsers(selectedRegion);
+      const schoolsPromise = this.populateSchools(selectedRegion);
+      const [{ data: usersData }, { data: schoolsData }] = await Promise.all(
+        [usersPromise,
+          schoolsPromise],
+      );
+      this.users = usersData;
+      this.schools = schoolsData;
       this.currentlySelectedRegion = selectedRegion;
-      this.findUsers({
-        query: {
-          _id: {
-            $in: this.currentlySelectedRegion.users,
-          },
-        },
-      }).then((response) => {
-        this.users = response.data;
-      });
-      this.findSchools({
-        query: {
-          _id: {
-            $in: this.currentlySelectedRegion.schools,
-          },
-        },
-      }).then((response) => {
-        this.schools = response.data;
-      });
     },
   },
   /* intialling fetching all the regions as a list when the page is created. Once
   the regions have been loaded, then the user can individually click on them */
-  created() {
-    this.findRegions({
-      query: {},
-    }).then((response) => {
-      this.allRegions = response.data;
-    });
+  async mounted() {
+    const { data: regionData } = await this.findRegions();
+    this.allRegions = regionData;
   },
 };
 </script>
-
-<style></style>
