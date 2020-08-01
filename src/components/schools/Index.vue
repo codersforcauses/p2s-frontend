a1b7c86cc991bafc2caacb7bf5eeb3a44bf443f0
Eddie Atkinson <22487668@student.uwa.edu.au>
2020-08-01 22:22:15 +0800

    starts adding nav drawer for selecting schools

--- src/components/schools/Index.vue
+++ src/components/schools/Index.vue
@@ -1,19 +1,35 @@
 <template>
   <div>
-    <ul>
-      <li
-      v-for='school in schools'
-      :key='school._id'
-      >
-        {{ school.name }}
-      </li>
-    </ul>
-    <v-container>
+    <v-container
+    fluid>
       <v-layout
-      justify-center
+      row
       >
+      <v-flex xs12>
         <v-card>
+          <v-card-title
+          class='headline ma-0'
+          >
+            Schools on File
+          </v-card-title>
+          <v-subheader>
+            Click a school to see its details
+          </v-subheader>
           <v-card-text>
+            <v-list>
+              <v-list-item
+              v-for='school in schools'
+              :key='school._id'
+              >
+                <v-list-tile-content>
+                  <v-list-tile-title
+                  v-text='school.name'
+                  class="text--primary"
+                  >
+                  </v-list-tile-title>
+                </v-list-tile-content>
+              </v-list-item>
+            </v-list>
             <v-pagination
             v-model='pagination.currentPage'
             :length='pagination.pageCount'
@@ -23,8 +39,42 @@
             </v-pagination>
           </v-card-text>
         </v-card>
+      </v-flex>
       </v-layout>
+      <v-navigation-drawer
+      v-model='drawer'
+      absolute
+      temporary
+      >
+        Hello world
+      </v-navigation-drawer>
+      <v-btn @click='drawer=!drawer'>Toggle</v-btn>
     </v-container>
+    <!-- <v-navigation-drawer
+      v-model='drawer'
+      >
+        {{ currentSchool.name }}
+            <v-list>
+              <v-list-tile>
+                <v-icon
+                left
+                class='material-icons'
+                >
+                  mdi-map-marker
+                </v-icon>
+                Address: {{ formatAddress(currentSchool.address) }}
+              </v-list-tile>
+              <v-list-tile>
+                <v-icon
+                left
+                class='material-icons'
+                >
+                  mdi-phone
+                </v-icon>
+                Phone number: {{ currentSchool.phoneNumber }}
+              </v-list-tile>
+            </v-list>
+      </v-navigation-drawer> -->
   </div>
 </template>
 
@@ -52,6 +102,8 @@ export default {
         maxButtons: 5,
       },
       schools: [],
+      currentSchool: null,
+      drawer: false,
     };
   },
   computed: {
@@ -73,10 +125,14 @@ export default {
       this.pagination.pageCount = Math.round(
         this.pagination.total / this.pagination.itemsPerPage,
       );
+      [this.currentSchool] = this.schools;
     },
     handlePageChange() {
       this.pagination.skip = (this.pagination.currentPage - 1) * this.pagination.itemsPerPage;
       this.fetchSchools();
+    },
+    formatAddress(address) {
+      return `${address.street}, ${address.suburb}, ${address.postcode}, ${address.state}`;
     },
   },
   mounted() {
@@ -86,5 +142,7 @@ export default {
 </script>
 
 <style>
-
+ .material-icons, .icon-text {
+      vertical-align: middle;
+    }
 </style>
